75afcf3c0b03d3ced1b836ecd6d87b0ba52c37b9
Kong Fanbo <xanked@gmail.com>
2021-03-30 23:54:56 +0800

    update model

--- flows.py
+++ flows.py
@@ -9,7 +9,7 @@ def predictFlows(data):
   # 数据清洗
   df = pd.json_normalize(data)
   dataset = df
-  # dataset['memberDay'] = dataset['memberDay']*1.0
+  dataset['memberDay'] = dataset['memberDay']*10000
   # dataset['isWeekend'] = dataset['isWeekend']*1.0
   # dataset['day'] = dataset['day']*1.0
 
